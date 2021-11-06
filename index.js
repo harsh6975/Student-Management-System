7bfb5442a5807d3e8f98ac1d68220ccf30a3204f
harsh6975 <harsh.sinha@iitg.ac.in>
2021-11-06 19:01:20 +0530

    layout and home controller added

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require("express");
-
+const Router = require("./routes/indexRoute");
+const layout = require("express-ejs-layouts");
 const app = express();
 const port = 3000;
 
+//layout
+app.use(layout);
+//extract styles and script from sub pages to layout
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
+
+//to encode data from form
+app.use(express.urlencoded());
+
+//connecting assets
+app.use(express.static("./assets"));
+
+//view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+//connecting Routes
+app.use("/", Router);
+
+//Listing to server
 app.listen(port, (err) => {
   if (err) {
     console.log("Error in loding express", err);
   }
-  console.log("Connected, listning to port ", port);
+  console.log("Connected, listening to port", port);
 });
